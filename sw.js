f6c6fd43b789292ce2f5c1b53adc2276d556d24f
RimmPost <RimmPost@yandex.ru>
2025-12-11 22:08:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,16 @@
-const CACHE_NAME = 'ozz-test-v2';
+const CACHE_NAME = 'ozz-test-v2'; // Обновил версию
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon.png'
+  './icon.png' // Добавил иконку в кэш (обязательно добавь файл в репозиторий)
 ];
 
+// Установка
 self.addEventListener('install', (e) => {
+  // Заставляем SW активироваться немедленно
+  self.skipWaiting();
+  
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
@@ -14,6 +18,22 @@ self.addEventListener('install', (e) => {
   );
 });
 
+// Активация
+self.addEventListener('activate', (e) => {
+  // Заставляем SW немедленно взять контроль над всеми вкладками
+  e.waitUntil(self.clients.claim());
+  
+  // Очистка старого кэша при смене версии
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch (Оффлайн режим)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
